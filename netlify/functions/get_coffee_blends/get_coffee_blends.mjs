a77df327be502173bbd1d5e0f47a6313e567cf23
zgl <zgl1@qq.com>
2025-11-04 17:11:59 +0800

    new

---
+++ netlify/functions/get_coffee_blends/get_coffee_blends.mjs
@@ -0,0 +1,19 @@
+// netlify/functions/get_coffee_blends/get_coffee_blends.mjs
+import { neon } from '@neondatabase/serverless';
+
+export async function handler(event) {
+  const sql = neon(process.env.DATABASE_URL);
+
+  try {
+    const rows = await sql.query('SELECT * FROM favorite_coffee_blends;');
+    return {
+      statusCode: 200,
+      body: JSON.stringify(rows),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error1: error.message }),
+    };
+  }
+}
